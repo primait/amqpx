62dbef03fa51399d851a895e4cc4000c94563b1f
larrywax <giuseppe.danna@prima.it>
2019-04-17 11:49:03 +0200

    Handle empty conf

--- lib/application.ex
+++ lib/application.ex
@@ -5,17 +5,27 @@ defmodule Amqpx.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    children = [] ++ consumers() ++ producers()
+    children =
+      consumers(Application.get_env(:amqpx, :consumers)) ++
+        producers(Application.get_env(:amqpx, :producers))
 
     opts = [strategy: :one_for_one, name: Amqpx.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  defp consumers() do
-    Enum.map(Application.get_env(:amqpx, :consumers), fn conf -> {Amqpx.Consumer, conf} end)
+  defp consumers(nil) do
+    []
   end
 
-  defp producers() do
-    Enum.map(Application.get_env(:amqpx, :producers), fn conf -> {Amqpx.Producer, conf} end)
+  defp consumers(configs) do
+    Enum.map(configs, fn conf -> {Amqpx.Consumer, conf} end)
+  end
+
+  defp producers(nil) do
+    []
+  end
+
+  defp producers(configs) do
+    Enum.map(configs, fn conf -> {Amqpx.Producer, conf} end)
   end
 end
