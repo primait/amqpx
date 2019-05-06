45f1d04ad75d4cd5e4db7070571471524a383d3c
larrywax <giuseppe.danna@prima.it>
2019-05-06 14:35:36 +0200

    CI pipeline

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Amqpx.MixProject do
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :production,
       deps: deps(),
+      aliases: aliases(),
       dialyzer: [
         plt_add_apps: [:mix],
         plt_add_deps: :transitive
@@ -23,13 +24,27 @@ defmodule Amqpx.MixProject do
     ]
   end
 
+  defp aliases do
+    [
+      check: [
+        "format --check-formatted mix.exs \"lib/**/*.{ex,exs}\" \"test/**/*.{ex,exs}\" \"priv/**/*.{ex,exs}\" \"config/**/*.{ex,exs}\"",
+        "credo",
+        "dialyzer --halt-exit-status"
+      ],
+      "format.all": [
+        "format mix.exs \"lib/**/*.{ex,exs}\" \"test/**/*.{ex,exs}\" \"priv/**/*.{ex,exs}\" \"config/**/*.{ex,exs}\""
+      ]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:amqp, "~> 1.1"},
       {:logger_logstash_backend, github: "primait/logger_logstash_backend", ref: "master"},
       {:dialyxir, "1.0.0-rc.4", only: :dev, runtime: false},
-      {:elixir_uuid, "~> 1.1"}
+      {:elixir_uuid, "~> 1.1"},
+      {:credo, "~> 1.0", only: [:dev, :test]}
     ]
   end
 end
