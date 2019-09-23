b5928ec08cad067bffe11deaec6a64ef4d1749e4
Michelangelo <michelangelo@morrillo.it>
2019-09-23 15:41:06 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Amqpx.MixProject do
     [
       app: :amqpx,
       name: "amqpx",
-      version: "4.0.1",
+      version: "5.0.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :production,
