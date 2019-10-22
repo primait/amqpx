73266ae623df6c1367af44c2f2a7d71552d0f51c
larrywax <giuseppe.danna@prima.it>
2019-10-22 16:29:33 +0200

    remove

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Amqpx.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      # mod: {Amqpx.Application, []},
       extra_applications: [:logger]
     ]
   end
