569aabed8b4e8b149556c696f865e816fe4e9b32
larrywax <giuseppe.danna@prima.it>
2021-02-10 09:18:36 +0100

    update

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -7,30 +7,47 @@ defmodule ConnectionTest do
   @obfuscate_password false
 
   test "open connection with host as binary" do
-    assert {:ok, conn} = Connection.open(host: "rabbit", obfuscate_password: @obfuscate_password)
+    assert {:ok, conn} =
+             Connection.open(
+               username: "amqpx",
+               password: "amqpx",
+               host: "rabbit",
+               obfuscate_password: @obfuscate_password
+             )
+
     assert :ok = Connection.close(conn)
   end
 
   test "open connection with host as char list" do
-    assert {:ok, conn} = Connection.open(host: 'rabbit', obfuscate_password: @obfuscate_password)
+    assert {:ok, conn} =
+             Connection.open(
+               username: "amqpx",
+               password: "amqpx",
+               host: 'rabbit',
+               obfuscate_password: @obfuscate_password
+             )
+
     assert :ok = Connection.close(conn)
   end
 
   test "open connection using uri" do
-    assert {:ok, conn} = Connection.open("amqp://rabbit", obfuscate_password: @obfuscate_password)
+    assert {:ok, conn} = Connection.open("amqp://amqpx:amqpx@rabbit", obfuscate_password: @obfuscate_password)
     assert :ok = Connection.close(conn)
   end
 
   test "open connection using both uri and options" do
     assert {:ok, conn} =
-             Connection.open("amqp://nonexistent:5672", host: 'rabbit', obfuscate_password: @obfuscate_password)
+             Connection.open("amqp://amqpx:amqpx@nonexistent:5672",
+               host: 'rabbit',
+               obfuscate_password: @obfuscate_password
+             )
 
     assert :ok = Connection.close(conn)
   end
 
   test "open connection with uri, name, and options" do
     assert {:ok, conn} =
-             Connection.open("amqp://nonexistent:5672", "my-connection",
+             Connection.open("amqp://amqpx:amqpx@nonexistent:5672", "my-connection",
                host: 'rabbit',
                obfuscate_password: @obfuscate_password
              )
@@ -39,11 +56,11 @@ defmodule ConnectionTest do
   end
 
   test "override uri with options" do
-    uri = "amqp://guest:guest@rabbit:5672"
+    uri = "amqp://guest:amqpx@rabbit:5672"
     {:ok, amqp_params} = uri |> String.to_charlist() |> :amqp_uri.parse()
 
     record =
-      Connection.merge_options_to_amqp_params(amqp_params, username: "guest", obfuscate_password: @obfuscate_password)
+      Connection.merge_options_to_amqp_params(amqp_params, username: "amqpx", obfuscate_password: @obfuscate_password)
 
     params = amqp_params_network(record)
 
