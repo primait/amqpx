23b751fbfa482cbef26b57493179739c7ec9b052
Stefano <stefano.candori@prima.it>
2022-07-08 17:36:34 +0200

    Add requeue_on_reject config option

---
+++ test/support/consumer/consumer_no_requeue.ex
@@ -0,0 +1,21 @@
+defmodule Amqpx.Test.Support.NoRequeueConsumer do
+  @moduledoc nil
+  @behaviour Amqpx.Gen.Consumer
+
+  alias Amqpx.Basic
+  alias Amqpx.Helper
+
+  def setup(channel) do
+    Helper.declare(channel, Application.fetch_env!(:amqpx, __MODULE__))
+    Basic.consume(channel, Application.fetch_env!(:amqpx, __MODULE__)[:queue], self())
+    {:ok, %{}}
+  end
+
+  def handle_message_rejection(_msg, _err) do
+    :ok
+  end
+
+  def handle_message(_payload, _meta, _state) do
+    raise "test error"
+  end
+end
