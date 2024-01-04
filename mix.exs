ee2be933f7e3361745400593639530769abc40ba
Simone Cottini <cottini.simone@gmail.com>
2024-01-04 10:47:48 +0100

    6.0.2-rc.0 (#178)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Amqpx.MixProject do
     [
       app: :amqpx,
       name: "amqpx",
-      version: "6.0.2",
+      version: "6.0.2-rc.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :production,
