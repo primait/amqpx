04574055a99ef7d708383fea3742ba87ba186003
Simone Cottini <cottini.simone@gmail.com>
2024-05-31 15:41:23 +0200

    Rename start_producer fn

--- test/gen_test.exs
+++ test/gen_test.exs
@@ -20,7 +20,7 @@ defmodule Amqpx.Test.AmqpxTest do
   test "e2e: should publish message and consume it" do
     start_connection1!()
     start_consumer_by_name!(Consumer1)
-    start_producer_by_name!(:producer)
+    start_producer!(:producer)
 
     payload = %{test: 1}
 
@@ -36,8 +36,8 @@ defmodule Amqpx.Test.AmqpxTest do
     start_connection2!()
     start_consumer_by_name!(Consumer1)
     start_consumer_by_name!(Consumer2)
-    start_producer_by_name!(:producer)
-    start_producer_by_name!(:producer2)
+    start_producer!(:producer)
+    start_producer!(:producer2)
 
     payload = %{test: 1}
     payload2 = %{test: 2}
@@ -59,7 +59,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
   test "e2e: try to publish to an exchange defined in producer conf" do
     start_connection1!()
-    start_producer_by_name!(:producer)
+    start_producer!(:producer)
     payload = %{test: 1}
 
     assert Producer3.send_payload(payload) === :ok
@@ -70,8 +70,8 @@ defmodule Amqpx.Test.AmqpxTest do
     start_connection2!()
     start_consumer_by_name!(Consumer1)
     start_consumer_by_name!(Consumer2)
-    start_producer_by_name!(:producer)
-    start_producer_by_name!(:producer2)
+    start_producer!(:producer)
+    start_producer!(:producer2)
 
     test_pid = self()
 
@@ -104,7 +104,7 @@ defmodule Amqpx.Test.AmqpxTest do
   test "e2e: should handle message rejected when handle message fails" do
     start_connection1!()
     start_consumer_by_name!(HandleRejectionConsumer)
-    start_producer_by_name!(:producer)
+    start_producer!(:producer)
 
     test_pid = self()
 
@@ -137,7 +137,7 @@ defmodule Amqpx.Test.AmqpxTest do
   test "e2e: messages should not be re-enqueued when re-enqueue option is disabled" do
     start_connection1!()
     start_consumer_by_name!(NoRequeueConsumer)
-    start_producer_by_name!(:producer)
+    start_producer!(:producer)
 
     test_pid = self()
     error_message = "test_error"
@@ -167,7 +167,7 @@ defmodule Amqpx.Test.AmqpxTest do
   test "e2e: handle_message_reject should be called upon first time when re-enqueue option is disabled" do
     start_connection1!()
     start_consumer_by_name!(NoRequeueConsumer)
-    start_producer_by_name!(:producer)
+    start_producer!(:producer)
 
     test_pid = self()
     error_message = "test-error-requeue"
@@ -211,8 +211,8 @@ defmodule Amqpx.Test.AmqpxTest do
     start_connection2!()
     start_consumer_by_name!(Consumer1)
     start_consumer_by_name!(ConsumerConnectionTwo)
-    start_producer_by_name!(:producer)
-    start_producer_by_name!(:producer_connection_two)
+    start_producer!(:producer)
+    start_producer!(:producer_connection_two)
 
     payload = %{test: 1}
 
@@ -232,7 +232,7 @@ defmodule Amqpx.Test.AmqpxTest do
   describe "when publish retry configurations are enabled" do
     test "should retry publish in case of publish error" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_publish_error)
+      start_producer!(:producer_with_retry_on_publish_error)
       payload = %{test: 1}
 
       with_mock(Amqpx.Basic,
@@ -254,7 +254,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
     test "should not retry publish in case of publish error if on_publish_error retry_policy is not set" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_publish_rejected)
+      start_producer!(:producer_with_retry_on_publish_rejected)
       payload = %{test: 1}
 
       with_mock(Amqpx.Basic,
@@ -269,7 +269,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
     test "should retry publish in case of publish rejection" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_publish_rejected)
+      start_producer!(:producer_with_retry_on_publish_rejected)
       payload = %{test: 1}
 
       with_mock(Amqpx.Confirm,
@@ -291,7 +291,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
     test "should not retry publish in case of publish rejection if on_publish_rejected retry_policy is not set" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_publish_error)
+      start_producer!(:producer_with_retry_on_publish_error)
       payload = %{test: 1}
 
       with_mock(Amqpx.Confirm,
@@ -306,7 +306,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
     test "should retry publish in case of confirm delivery timeout" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_confirm_delivery_timeout)
+      start_producer!(:producer_with_retry_on_confirm_delivery_timeout)
       payload = %{test: 1}
 
       with_mocks([
@@ -344,7 +344,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
     test "should retry publish in case of confirm delivery timeout and on publish error" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_retry_on_confirm_delivery_timeout_and_on_publish_error)
+      start_producer!(:producer_with_retry_on_confirm_delivery_timeout_and_on_publish_error)
       payload = %{test: 1}
 
       with_mocks([
@@ -407,7 +407,7 @@ defmodule Amqpx.Test.AmqpxTest do
 
   test "test retry configurations" do
     start_connection1!()
-    start_producer_by_name!(:producer_with_retry_on_publish_error)
+    start_producer!(:producer_with_retry_on_publish_error)
     payload = %{test: 1}
 
     with_mock(Amqpx.Basic,
@@ -434,7 +434,7 @@ defmodule Amqpx.Test.AmqpxTest do
   describe "when publish retry configurations are not enabled" do
     test "should not retry publish in case of error" do
       start_connection1!()
-      start_producer_by_name!(:producer)
+      start_producer!(:producer)
       payload = %{test: 1}
 
       with_mock(Amqpx.Basic,
@@ -492,7 +492,7 @@ defmodule Amqpx.Test.AmqpxTest do
   describe "jittered backoff validation" do
     test "should be invoked the configured number of times" do
       start_connection1!()
-      start_producer_by_name!(:producer_with_jittered_backoff)
+      start_producer!(:producer_with_jittered_backoff)
       payload = %{test: 1}
 
       with_mocks([
@@ -590,7 +590,7 @@ defmodule Amqpx.Test.AmqpxTest do
     :timer.sleep(@start_supervised_timeout)
   end
 
-  defp start_producer_by_name!(name) when is_atom(name) do
+  defp start_producer!(name) when is_atom(name) do
     start_supervised!(%{
       id: name,
       start: {Amqpx.Gen.Producer, :start_link, [Application.fetch_env!(:amqpx, name)]}
