69ac538305e8124f65b34ad3ca319052345451ff
John Bell <bell.john.andrew@gmail.com>
2024-11-29 09:43:38 +0000

    Code review: fix typo

--- lib/amqp/no_signal_handler.ex
+++ lib/amqp/no_signal_handler.ex
@@ -3,7 +3,7 @@ defmodule Amqpx.NoSignalHandler do
   Dummy signal handler module that does not handle the graceful termination.
 
   It always returns `false` for `draining?/0` and `stopping?/0`.
-  I.e. the comsumer will continue without handling signals.
+  I.e. the consumer will continue without handling signals.
   """
 
   @behaviour Amqpx.SignalHandler
